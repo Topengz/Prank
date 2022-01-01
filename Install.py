57c9211855e635372e9dba06233c8eaf880b64e9
Secret ID <92369060+Topengz@users.noreply.github.com>
2022-01-01 15:22:59 +0800

    Update Install.py

--- Install.py
+++ Install.py
@@ -1,7 +1,7 @@
 from os import system as S
 S('apt update && apt upgrade -y')
-S('apt install python -y')
+S('apt install wget python -y')
 S('pip install --upgrade pip')
 S('pip install faker')
-S('apt install requests wget')
+S('pip install requests')
 S('python prank')
